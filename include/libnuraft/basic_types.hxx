75017885cd0ad5f10c805f035fa5b44ae991aa3a
sheepgrass <sheepgrass@gmail.com>
2020-10-21 10:09:40 +0800

    add build to .gitignore and  enclose typedefs with namespace in basic_type.hxx (#146)
    * add build to .gitignore
    
    * enclose typedefs with namespace in basic_type.hxx
    
    Co-authored-by: Curtis Lo <curtis.lo@gwghk.com>
    Co-authored-by: Jung-Sang Ahn <jungsang.ahn@gmail.com>

--- include/libnuraft/basic_types.hxx
+++ include/libnuraft/basic_types.hxx
@@ -23,6 +23,8 @@ limitations under the License.
 
 #include <cstdint>
 
+namespace nuraft {
+
 typedef uint64_t ulong;
 typedef int64_t int64;
 typedef void* any_ptr;
@@ -30,5 +32,7 @@ typedef unsigned char byte;
 typedef uint16_t ushort;
 typedef uint32_t uint;
 typedef int32_t int32;
+
+}
 
 #endif // _BASIC_TYPES_HXX_
