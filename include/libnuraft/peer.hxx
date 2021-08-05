284865ca7aaf1d697732b77ed1b65d8699493ff5
Jung-Sang Ahn <jungsang.ahn@gmail.com>
2021-08-04 23:14:35 -0700

    Fix to bug in `get_peer_info`
    * `get_next_log_idx` may not be accurate if we want to get the last
    log successfully sent to the peer.

--- include/libnuraft/peer.hxx
+++ include/libnuraft/peer.hxx
@@ -48,6 +48,7 @@ public:
         , rpc_backoff_( ctx.get_params()->rpc_failure_backoff_ )
         , max_hb_interval_( ctx.get_params()->max_hb_interval() )
         , next_log_idx_(0)
+        , last_accepted_log_idx_(0)
         , next_batch_size_hint_in_bytes_(0)
         , matched_idx_(0)
         , busy_flag_(false)
@@ -149,6 +150,14 @@ public:
 
     void set_next_log_idx(ulong idx) {
         next_log_idx_ = idx;
+    }
+
+    uint64_t get_last_accepted_log_idx() const {
+        return last_accepted_log_idx_;
+    }
+
+    void set_last_accepted_log_idx(uint64_t to) {
+        last_accepted_log_idx_ = to;
     }
 
     int64 get_next_batch_size_hint_in_bytes() const {
@@ -343,6 +352,11 @@ private:
      * Next log index of this server.
      */
     std::atomic<ulong> next_log_idx_;
+
+    /**
+     * The last log index accepted by this server.
+     */
+    std::atomic<uint64_t> last_accepted_log_idx_;
 
     /**
      * Hint of the next log batch size in bytes.
