6925f4f12af3ecabde2d302b0c42b02a1434de1a
JackyWoo <wuchienchao@qq.com>
2022-02-28 02:26:18 +0800

    Fix log output (#290)
    * fix log output
    
    * fix log

--- src/raft_server.cxx
+++ src/raft_server.cxx
@@ -1032,9 +1032,9 @@ bool raft_server::check_leadership_validity() {
     }
     int32 min_quorum_size = get_quorum_for_commit() + 1;
     if ( (num_voting_members - nr_peers) < min_quorum_size ) {
-        p_er("%zu nodes (out of %zu, %zu including learners) are not "
-             "responding longer than %zu ms, "
-             "at least %zu nodes (including leader) should be alive "
+        p_er("%d nodes (out of %d, %zu including learners) are not "
+             "responding longer than %d ms, "
+             "at least %d nodes (including leader) should be alive "
              "to proceed commit",
              nr_peers,
              num_voting_members,
