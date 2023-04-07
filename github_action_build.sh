6bb7b0e0282d12f47c0f9cecf09b1459012d8d77
Lucas Gonze <lucas@gonze.com>
2023-04-07 14:01:28 -0700

    Set workflow permissions to read-only. See https://github.com/ossf/scorecard/blob/e42af756609b2cde6d757fd45ea05ddf0016ff62/docs/checks.md#token-permissions
    Execute code coverage metrics in an action rather than in github_build_action.sh and travis_build.sh. See https://github.com/ossf/scorecard/blob/e42af756609b2cde6d757fd45ea05ddf0016ff62/docs/checks.md#pinned-dependencies
    
    Enabling Dependabot version updates for GitHub Actions.

--- github_action_build.sh
+++ github_action_build.sh
@@ -15,7 +15,6 @@ echo "branch name: ${BRANCH_NAME}"
 if [ -z "${GITHUB_HEAD_REF}" ]; then
     cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=1 -DENABLE_RAFT_STATS=1 ../
     make -j2 raft_cov
-    bash <(curl -s https://codecov.io/bash) -f raft_cov.info.cleaned
 else
     echo "pull request sha: ${GITHUB_SHA}"
     cmake -DCMAKE_BUILD_TYPE=Debug -DADDRESS_SANITIZER=1 -DENABLE_RAFT_STATS=1 ../
--- travis_build.sh
+++ travis_build.sh
@@ -8,7 +8,6 @@ if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo "branch: ${TRAVIS_BRANCH}"
     cmake -DCMAKE_BUILD_TYPE=Debug -DCODE_COVERAGE=1 -DENABLE_RAFT_STATS=1 ../
     make -j2 raft_cov
-    bash <(curl -s https://codecov.io/bash) -f raft_cov.info.cleaned
 else
     echo "pull request sha: ${TRAVIS_PULL_REQUEST_SHA}"
     cmake -DCMAKE_BUILD_TYPE=Debug -DADDRESS_SANITIZER=1 -DENABLE_RAFT_STATS=1 ../
